c3c33f4457b93f15e08283b8b48f29b0d82f590c
qbix213 <gryigry3@gmail.com>
2020-09-12 00:28:46 +0200

    Add website

---
+++ js/typeanimation.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed('.typed', {
+    strings: ["Subscribe to quenadares.", "Join Liptonawka.","Enjoy." ],
+    typeSpeed: 80,
+    loop: true,
+    backSpeed: 40,
+    backDelay: 4000
+    });
